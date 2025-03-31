1cfb37c74d9ea36f5345ea5cadf81927b07afffd
antucpa <bisteccaditopo@gmail.com>
2025-03-31 11:02:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -17,3 +18,7 @@ def get_followers():
         return str(count)
     except:
         return "-1"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
